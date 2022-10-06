b0a020a2c59fbbcf70d1f0110b312f8ac8ea8dd9
JoseEduardoPizano <42656826+JoseEduardoPizano@users.noreply.github.com>
2022-10-05 20:07:45 -0500

    Add files via upload

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -12,6 +12,44 @@ class TestZookeeper(unittest.TestCase):
         with self.assertRaises(Exception):
             tree = Ztree()
             tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
+            
+    def test_1_znode(self):
+        tree = Ztree()
+        tree.create('/node1', 'hola', True, True, 10, '/')   
+        tree.create('/node2', 'que', True, True, 10, '/node1')
+        tree.create('/node3', 'hace', True, True, 10, '/node2')
+        self.assertEqual(tree.getData('/node1'), 'hola')
+        self.assertEqual(tree.getData('/node2'), 'que')
+        self.assertEqual(tree.getData('/node3'), 'hace')
+        
+    def test_2_znode(self):
+        tree = Ztree()
+        tree.create('/node1', 'hola', True, True, 10, '/')   
+        tree.create('/node2', 'que', True, True, 10, '/')
+        tree.create('/node3', 'hace', True, True, 10, '/')
+        self.assertEqual(tree.getData('/node1'), 'hola')
+        self.assertEqual(tree.getData('/node2'), 'que')
+        self.assertEqual(tree.getData('/node3'), 'hace')
+        
+    def test_3_znode(self):
+        tree = Ztree()
+        tree.create('/node1', 'hola', True, True, 10, '/')   
+        tree.create('/node2', 'que', True, True, 10, '/node1')
+        tree.create('/node3', 'hace', True, True, 10, '/node2')
+        print(tree.delete('/node3',0))
+        
+    def test_4_znode(self):
+        tree = Ztree()
+        tree.create('/node1', 'hola', True, True, 10, '/')   
+        tree.create('/node2', 'que', True, True, 10, '/node1')
+        print(tree.delete('/node3',0))
+        
+    def test_5_znode(self):
+        with self.assertRaises(Exception):
+            tree = Ztree()
+            tree.create('/node1', 'hola', True, True, 10, '/')   
+            tree.create('/node2', 'que', True, True, 10, '/node5')
+            
 
 
 if __name__ == '__main__':
